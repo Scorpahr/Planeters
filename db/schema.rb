bbf205ee812f121cc2d00e75164c852d96592fbc
stephane <stephane@wivzem.fr>
2024-05-27 17:38:33 +0200

    database error fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_27_125344) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_27_153234) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,11 +64,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_27_125344) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "nickname"
-    t.string "firstname"
-    t.string "lastname"
-    t.string "password_digest"
-    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
