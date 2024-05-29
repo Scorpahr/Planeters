2942eb5f93e2feded3ee1a8a5df1edb7e3547604
stephane <stephane@wivzem.fr>
2024-05-29 11:29:19 +0200

    add authentication to be able to see the add button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  protected
+
+  def after_sign_in_path_for(resource)
+    # vérifie si notre user est connecté. S'il l'est, l'url est engeristrée. Sinon, on redirige vers la page d'accueil.
+    stored_location_for(resource) || root_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    # Redirige l'utilisateur vers la page d'accueil après la déconnexion.
+    root_path
+  end
 end
